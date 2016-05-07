02b148a12f8640a432ee9ea711346d587b096359
Kenneth D. Evensen <kevensen@redhat.com>
2016-05-07 11:21:55 -0400

    forgot s3 subcommand

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws $AWS_ARGS cp s3://${IN_BUCKET}/ $HOME/output/ $AWS_S3_ARGS
-aws $AWS_ARGS cp $HOME/output/ s3://${OUT_BUCKET}/ $AWS_S3_ARGS
+aws ${AWS_ARGS} s3 cp s3://${IN_BUCKET}/ $HOME/output/ ${AWS_S3_ARGS}
+aws ${AWS_ARGS} s3 cp $HOME/output/ s3://${OUT_BUCKET}/ ${AWS_S3_ARGS}
